b42d952b49a25503412acaff5dba8ba6c48296a9
Luqman Hakim <leekim1170@gmail.com>
2023-05-31 22:52:49 +0800

    update on index.js
    update on marriage consultation at index.js

---
+++ view/pages/marriage_consultation/index.js
@@ -0,0 +1,12 @@
+import {
+    Box
+} from "@mui/material";
+import CheckCons from "./check_consultation";
+
+export default function MarriageCons() {
+    return (
+        <Box>
+            <CheckCons />
+        </Box>
+    );
+}

6ae3c222468fde54f73b8234cb9c08c155c3b25c
Luqman Hakim <leekim1170@gmail.com>
2023-06-01 01:31:26 +0800

    add apply consultation
    this actually for date consultation but i'll change the folder name later when i get help

--- view/pages/marriage_consultation/index.js
+++ view/pages/marriage_consultation/index.js
@@ -2,11 +2,15 @@ import {
     Box
 } from "@mui/material";
 import CheckCons from "./check_consultation";
+import ApplyCons from "./apply_consultation";
 
+//this is marriage consultation package view
 export default function MarriageCons() {
+
     return (
         <Box>
             <CheckCons />
+            <ApplyCons />
         </Box>
     );
 }

7904bb01a753772ee8f8efcd4f5aa9613a43cc0c
erbakryyy12 <erniemastura09@gmail.com>
2023-06-01 01:26:16 +0800

    incentive

---
+++ view/pages/incentive/index.js
@@ -0,0 +1,12 @@
+import {
+    Box
+} from "@mui/material";
+import incentive_status from "./incentive_status";
+
+export default function incentive() {
+    return (
+        <Box>
+            <incentive_status />
+        </Box>
+    );
+}

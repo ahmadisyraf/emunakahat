ff0d5eda6131c2054ad540694fa3a4009101183b
Luqman Hakim <leekim1170@gmail.com>
2023-06-12 19:18:38 +0800

    add controller and model for marriage consultation module

---
+++ backend/model/marriage_consultation.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+    MC_ID: {
+        required: false,
+        type: String,
+    },
+    USER_IC: {
+        required: false,
+        type: String,
+    },
+    USER_PARTNER_ID: {
+        required: false,
+        type: String,
+    },
+    STAFF_ID: {
+        required: false,
+        type: String,
+    },
+    MC_COMPLAINT_PURPOSE: {
+        required: false,
+        type: String,
+    },
+    MC_COMPLAINT_STATEMENT: {
+        required: false,
+        type: String,
+    },
+    MC_ADVISOR_STATEMENT: {
+        required: false,
+        type: String,
+    },
+    MC_TIME_SLOT: {
+        required: false,
+        type: String,
+    },
+    MC_CONSULTATION_DATE: {
+        required: false,
+        type: String,
+    }
+
+})
+
+module.exports = mongoose.model('MarriageConsultation', courseSchema)
